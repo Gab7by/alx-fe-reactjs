885b18f283899f23153ed59ea2ce6d650654edc1
Gabriel Owusu <owusugabriel803@gmail.com>
2025-11-08 14:39:46 +0000

    counter

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,7 +6,7 @@ function Counter() {
         <div>
             <p>Current Count: {count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decreament</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
         </div>
     )
