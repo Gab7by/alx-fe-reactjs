14e48d10f32d4bce041971b709a3d3dbafd69937
Nana Kwame <owusugabriel803@gmail.com>
2025-12-16 18:39:44 +0000

    project 4

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+      <TodoList />
+      <AddTodoForm />
+    </>
+  );
+}
+
+export default App;
